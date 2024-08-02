f3be880e61e932aa2a078a63e4c15d95da74a3ce
abdulrahman-elfeky <abdulrahmanelfeky7@gmail.com>
2024-08-02 22:54:32 +0300

    fix card service

--- MoneyTransfer/src/main/java/com/BM/MoneyTransfer/service/CardService.java
+++ MoneyTransfer/src/main/java/com/BM/MoneyTransfer/service/CardService.java
@@ -6,11 +6,16 @@ import java.util.List;
 
 public interface CardService {
     Card addCard(Card card);
+
     Card getCard(String cardNumber);
+
     List<Card> getAllCards();
+
     Card updateCard(Card card);
+
     void deleteCard(String cardNumber);
 
     Double getCardBalance(String cardNumber); // Method to get card balance
+
     void updateCardBalance(String cardNumber, Double newBalance); // Method to update card balance
 }
--- MoneyTransfer/src/main/java/com/BM/MoneyTransfer/service/CardServiceImpl.java
+++ MoneyTransfer/src/main/java/com/BM/MoneyTransfer/service/CardServiceImpl.java
@@ -1,9 +1,16 @@
 package com.BM.MoneyTransfer.service;
 
 import com.BM.MoneyTransfer.dao.CardDao;
+import com.BM.MoneyTransfer.dao.UserDao;
 import com.BM.MoneyTransfer.entity.Card;
+import com.BM.MoneyTransfer.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,14 +18,25 @@ import java.util.List;
 public class CardServiceImpl implements CardService {
 
     private final CardDao cardDao;
+    private final UserDao userDao;
 
     @Autowired
-    public CardServiceImpl(CardDao cardDao) {
+    public CardServiceImpl(CardDao cardDao, UserDao userDao) {
         this.cardDao = cardDao;
+        this.userDao = userDao;
+    }
+
+    public String getCurrentUserEmail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return userDetails.getUsername();
     }
 
     @Override
+    @Transactional
     public Card addCard(Card card) {
+        String email = getCurrentUserEmail();
+        card.setUser(userDao.findById(email).orElse(null));
         return cardDao.save(card);
     }
 
@@ -28,8 +46,10 @@ public class CardServiceImpl implements CardService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Card> getAllCards() {
-        return cardDao.findAll();
+        User user = userDao.findById(getCurrentUserEmail()).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return user.getCards();
     }
 
     @Override
