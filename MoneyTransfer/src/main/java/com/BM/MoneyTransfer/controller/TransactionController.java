bffe3d426ccd880531d8d0b30ba3f3f3d5962707
Y-Elsayed <108357222+Y-Elsayed@users.noreply.github.com>
2024-08-02 17:55:35 +0300

    Transaction Controller Done

--- MoneyTransfer/src/main/java/com/BM/MoneyTransfer/controller/TransactionController.java
+++ MoneyTransfer/src/main/java/com/BM/MoneyTransfer/controller/TransactionController.java
@@ -1,29 +1,42 @@
-//package com.BM.MoneyTransfer.controller;
-//
-//import com.BM.MoneyTransfer.dto.TransactionDto;
-//import com.BM.MoneyTransfer.entity.Transaction;
-//import com.BM.MoneyTransfer.service.TransactionService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api")
-//public class TransactionController {
-//
-//    @Autowired
-//    private TransactionService transactionService;
-//
-//    @GetMapping("/transactions")
-//    public ResponseEntity<List<Transaction>> getTransactions(@RequestParam String cardNumber) {
-//        return ResponseEntity.ok().build();
-//    }
-//
-//    // Transfer money
-//    @PostMapping("/transfer")
-//    public ResponseEntity<Transaction> transferMoney(@RequestBody TransactionDto transactionDto) {
-//        return ResponseEntity.ok().build();
-//    }
-//}
+package com.BM.MoneyTransfer.controller;
+
+import com.BM.MoneyTransfer.response.TransactionResponse;
+import com.BM.MoneyTransfer.service.TransactionService;
+import com.BM.MoneyTransfer.entity.Transaction;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@RestController
+@RequestMapping("/api")
+@AllArgsConstructor
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @GetMapping("/transactions")
+    public Page<Transaction> getTransactionHistory(Pageable pageable) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userEmail = authentication.getName();
+
+        return transactionService.findByEmail(userEmail, pageable);
+    }
+
+    @PostMapping("/transfer")
+    public ResponseEntity<TransactionResponse> createTransaction(@RequestBody Transaction transaction) {
+        // Get the authenticated user's email
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userEmail = authentication.getName();
+
+        transaction.setSenderEmail(userEmail);
+
+        Transaction savedTransaction = transactionService.save(transaction);
+
+        return ResponseEntity.ok(new TransactionResponse(savedTransaction, "Transaction approved"));
+    }
+}
