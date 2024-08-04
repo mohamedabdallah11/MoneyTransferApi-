01c1d011f54029dec3a0a5fc70ba018059a9d7a5
mohamed abdallah <mohamedabdallahh26@gmail.com>
2024-08-04 17:27:48 +0300

    setup the TransactioncController testing

---
+++ MoneyTransfer/src/test/java/com/BM/MoneyTransfer/controller/unitTesting/TransactionControllerTest.java
@@ -0,0 +1,59 @@
+package com.BM.MoneyTransfer.controller.unitTesting;
+import com.BM.MoneyTransfer.controller.TransactionController;
+import com.BM.MoneyTransfer.dto.TransferResponseDTO;
+import com.BM.MoneyTransfer.entity.Transaction;
+import com.BM.MoneyTransfer.dto.enums.Status;
+import com.BM.MoneyTransfer.service.TransactionService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Date;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class TransactionControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private TransactionService transactionService;
+
+    @InjectMocks
+    private TransactionController transactionController;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+        this.mockMvc = MockMvcBuilders
+                .standaloneSetup(transactionController)
+                .build();
+    }
+
+
+
+}
