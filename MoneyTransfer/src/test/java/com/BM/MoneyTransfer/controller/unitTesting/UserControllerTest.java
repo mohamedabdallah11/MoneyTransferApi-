0a3301cde8d677665c8d0a0c61bb8e714340bbd9
mohamed abdallah <mohamedabdallahh26@gmail.com>
2024-08-04 16:50:19 +0300

    initialize the UnitTesting of User

---
+++ MoneyTransfer/src/test/java/com/BM/MoneyTransfer/controller/unitTesting/UserControllerTest.java
@@ -0,0 +1,31 @@
+package com.BM.MoneyTransfer.controller.unitTesting;
+import static org.mockito.ArgumentMatchers.any;
+import com.BM.MoneyTransfer.dto.UserLoginRequestDTO;
+import com.BM.MoneyTransfer.entity.User;
+import com.BM.MoneyTransfer.service.JwtService;
+import com.BM.MoneyTransfer.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+public class UserControllerTest {
+}
